58ddb7979b7306960fe1eb2bc79bdb193295fd2c
Maxim Belov <belov1988@gmail.com>
2021-06-02 16:32:58 +0300

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+import { ChartType } from 'chart.js';
+
+declare module 'chart.js' {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface PluginOptionsByType<TType extends ChartType> {
+        colorschemes: { scheme: string } | string[];
+    }
+}
